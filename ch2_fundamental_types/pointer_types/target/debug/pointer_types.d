abc2a02d231b9981f923aa5a85d673ace32da6f3
jumzzz <jummel19@gmail.com>
2022-06-22 19:04:54 +0800

    delete

--- ch2_fundamental_types/pointer_types/target/debug/pointer_types.d
+++
@@ -1,1 +0,0 @@
-/home/jummel/project/rust/fast-safe/ch2_fundamental_types/pointer_types/target/debug/pointer_types: /home/jummel/project/rust/fast-safe/ch2_fundamental_types/pointer_types/src/main.rs
--- ch2_fundamental_types/tuple_types/target/debug/tuple_types.d
+++
@@ -1,1 +0,0 @@
-/home/jummel/project/rust/fast-safe/ch2_fundamental_types/tuple_types/target/debug/tuple_types: /home/jummel/project/rust/fast-safe/ch2_fundamental_types/tuple_types/src/main.rs
