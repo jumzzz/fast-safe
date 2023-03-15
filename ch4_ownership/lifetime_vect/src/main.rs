b61c1be23c01a445f238ac16a0c899aa0cac15f1
jumzzz <jummel19@gmail.com>
2023-03-14 17:54:00 -0700

    yes

--- ch4_ownership/lifetime_vect/src/main.rs
+++ ch4_ownership/lifetime_vect/src/main.rs
@@ -1,3 +1,25 @@
 fn main() {
-    println!("i dont know bro");
+    let mut v = Vec::new();
+    for i in 101..106 {
+        v.push(i.to_string());
+    }
+
+    println!("v0 = {:?}\n", &v);
+    
+    let fifth = v.pop().unwrap();
+    println!("after v.pop().unwrap()");
+    println!("v1 = {:?}", &v);
+    println!("fifth = {:?}\n", fifth);
+
+
+    let second = v.swap_remove(1);
+    println!("after v.swap_remove(1)");
+    println!("v2 = {:?}", &v);
+    println!("second = {:?}\n", second);
+    
+    let third = std::mem::replace(&mut v[2], "substitute".to_string());
+    println!("after std::mem::replace(&mut v[2], \"substitute\".to_string())");
+    println!("v3 = {:?}", &v);
+    println!("third = {:?}", third);
+
 }
