15f313751bc46566797a3f6f5f6e9d2c2c810c0b
jumzzz <jummel19@gmail.com>
2023-03-17 11:33:14 -0700

    yes

--- ch4_ownership/lifetime_vect/src/main.rs
+++ ch4_ownership/lifetime_vect/src/main.rs
@@ -1,4 +1,6 @@
-fn main() {
+#[allow(dead_code)]
+fn sample_v1() {
+
     let mut v = Vec::new();
     for i in 101..106 {
         v.push(i.to_string());
@@ -20,14 +22,39 @@ fn main() {
     println!("after std::mem::replace(&mut v[2], \"substitute\".to_string())");
     println!("v3 = {:?}", &v);
     println!("third = {:?}", third);
+}
+
+#[allow(dead_code)]
+fn sample_v2() {
 
     let x = vec!["1", "2", "3", "4"];
-
     // This needs &x
     for xx in &x {
         println!("{}", xx);
     }
-
     println!("{:?}", &x);
+}
+
+#[allow(dead_code)]
+fn sample_v3() {
+    let mut v = vec![
+        Some("x1".to_string()),
+        Some("x2".to_string()),
+        Some("x3".to_string()),
+    ];
+
+    let v0 = std::mem::replace(&mut v[0], None);
+    let v1 = std::mem::replace(&mut v[1], None);
+    let v2 = std::mem::replace(&mut v[2], None);
+
+    println!("v = {:?}", &v);
+    println!("v0 = {:?}", &v0);
+    println!("v1 = {:?}", &v1);
+    println!("v2 = {:?}", &v2);
+}
+
+
+fn main() {
+    sample_v3();
 
 }
