e7381d3c0b617d5f75d61e021c9fda0c16535cbf
jumzzz <jummel19@gmail.com>
2023-03-19 10:35:35 +0800

    Additional notes

--- ch5_references/ref_struct/src/main.rs
+++ ch5_references/ref_struct/src/main.rs
@@ -2,11 +2,18 @@ struct S<'a> {
     r: &'a i32,
 }
 
+struct D<'a> {
+    s: S<'a>,
+}
+
 fn main() {
     let x: i32 = 32;
-    let v = S {
-        r: &x,
+
+    let d = D {
+        s: S {
+            r: &x,
+        },
     };
 
-    println!("S.r = {:?}", &v.r);
+    println!("S.r = {:?}", &d.s.r);
 }
